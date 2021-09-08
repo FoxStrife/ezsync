4d2a1c1d7ae3f92af215e837d88ca7c536503651
FoxStrife <foxstrife@gmail.com>
2021-09-08 11:48:40 -0300

    main

--- Principal.py
+++ Principal.py
@@ -5,6 +5,11 @@ import json
 class Principal:
 
     cnpj = PropertyReader().readProperty('ClientSection','CNPJ')
+    sistema = PropertyReader().readProperty('ClientSection','SISTEMA')
+    integracao = PropertyReader().readProperty('IntegrationSection','INTEGRACAO')
+    user_integracao = PropertyReader().readProperty('IntegrationSection','USER')
+    pwd_integracao = PropertyReader().readProperty('IntegrationSection','PWD')
+    token_integracao = PropertyReader().readProperty('IntegrationSection','TOKEN')
     banco = PropertyReader().readProperty('DatabaseSection','BANCO')
     caminho = PropertyReader().readProperty('DatabaseSection','CAMINHO')
     usuario = PropertyReader().readProperty('DatabaseSection','USER')
@@ -20,8 +25,7 @@ class Principal:
 # conecta no tipo de banco que o cliente possui e extrai informacoes sobre a fila que será integrada
     if banco == 'firebird':
         #se o banco for Firebird
-        print ('Banco = '+ banco)
-        result= conectarFB('',caminho,usuario,senha,"SELECT FIRST 1 DISTINCT A.IDFILA, (SELECT FIRST 1 ID FROM RHPROTINTEGRATERC WHERE SYNC = 0 AND IDFILA = A.IDFILA), F.DESCRICAO FROM RHPROTINTEGRATERC A LEFT JOIN RHSISTEMAINTEGRATERCFILA F ON F.IDSISTEMA = A.IDSISTEMA WHERE  SYNC = 0")
+        result= selectFB('',caminho,usuario,senha,"SELECT FIRST 1 DISTINCT A.IDFILA, (SELECT FIRST 1 ID FROM RHPROTINTEGRATERC WHERE SYNC = 0 AND IDFILA = A.IDFILA), F.DESCRICAO FROM RHPROTINTEGRATERC A LEFT JOIN RHSISTEMAINTEGRATERCFILA F ON F.IDSISTEMA = A.IDSISTEMA WHERE  SYNC = 0")
         print(result)
         for r in result:
             filars = str(r[0])
@@ -29,16 +33,16 @@ class Principal:
             fila = r[2]
     elif banco == 'sqlserver':
         #se o banco for SQL Server
-        print ('Banco = '+ banco)
+        print()
     else:
         print ('Banco de dados não suportado.')
 
-    sql = conectarSQL(caminhoSQL,base,usuarioSQL,senhaSQL,"SELECT CAST(SQL AS VARCHAR(MAX)) AS SQL FROM DW_SQLS WHERE ID_FILA = "
+    sql = selectSQL(caminhoSQL,base,usuarioSQL,senhaSQL,"SELECT CAST(SQL AS VARCHAR(MAX)) AS SQL FROM DW_SQLS WHERE ID_FILA = "
     + str(filars) + " AND ID_SISTEMA_ORIGEM = 4 AND ID_SISTEMA_DESTINO = 1 AND ID_BANCO_DE_DADOS = " + str(3) + "")
     for s in sql:
         sql1 = str(s[0])
 
-    condicao = conectarSQL(caminhoSQL,base,usuarioSQL,senhaSQL,"SELECT CAST(CONDICAO AS VARCHAR(MAX)) AS CONDICAO FROM DW_SQLS WHERE ID_FILA = "
+    condicao = selectSQL(caminhoSQL,base,usuarioSQL,senhaSQL,"SELECT CAST(CONDICAO AS VARCHAR(MAX)) AS CONDICAO FROM DW_SQLS WHERE ID_FILA = "
     + str(filars) + " AND ID_SISTEMA_ORIGEM = 4 AND ID_SISTEMA_DESTINO = 1 AND ID_BANCO_DE_DADOS = " + str(3) + "")
     for s in condicao:
         condicao1 = str(s[0])
@@ -48,19 +52,19 @@ class Principal:
 
     if banco == 'firebird':
         #se o banco for Firebird
-        print ('Banco = '+ banco)
-        result2= conectarFB('',caminho,usuario,senha,condicao1 + filars)
+        result2= selectFB('',caminho,usuario,senha,condicao1 + filars)
         for r in result2:
             result3 = str(r[0])
         print (result3)
-        result4 = conectarFBJson('',caminho,usuario,senha,sql1 + result3)
+        result4 = selectFBJson('',caminho,usuario,senha,sql1 + result3)
         for r in result4:
             result5 = json.dumps(result4, indent=4)
-        print (result5)
-        
+        print (result5)        
     elif banco == 'sqlserver':
         #se o banco for SQL Server
-        print ('Banco = '+ banco)
+        print()
+
+    result6 = executeSQL('',caminho,usuario,senha,'EXEC DW_HUB ',filars,user_integracao,pwd_integracao,token_integracao,sistema,result5)
 
     #print ("ID =",filars)
     #print ("LIST =",pkdwsync)
